48747c4b70871581b5d34ccf651a9e2ec1ad26ac
Shaun Dashjian <shaundashjian@yahoo.com>
2017-06-18 15:16:16 -0600

    Refactored all course data to the services

--- ngLearningCenter/WebContent/app/course/courseLaunch/courseLaunch.component.js
+++ ngLearningCenter/WebContent/app/course/courseLaunch/courseLaunch.component.js
@@ -1,41 +1,14 @@
 angular.module('course')
 .component('courseLaunch',{
 	templateUrl : 'app/course/courseLaunch/courseLaunch.component.html',
-	controller : function(){
+	controller : function($routeParams, courseLaunchService, courseService){
 		var vm = this;
 		
-		vm.steps = [{
-			stepNo : 1,
-			title : 'Acceleration Clause',
-			description : 'A clause in your mortgage which allows the lender to demand payment of the outstanding loan balance for various reasons. The most common reasons for accelerating a loan are if the borrower defaults on the loan or transfers title to another individual without informing the lender.',
-			imageUrl : 'http://notequeen.com/wp-content/uploads/sites/45/2008/11/halloween.jpg',
-			question : 'The acceleration clause allows the lender to demand payment.',
-			answer : 'A'
-		}, {
-			stepNo : 2,
-			title : 'Adjustable-Rate Mortgage (ARM)',
-			description : 'A mortgage in which the interest changes periodically, according to corresponding fluctuations in an index. All ARMs are tied to indexes.',
-			imageUrl : 'http://www.themoneyalert.com/images/Adjustable_Rate_Mortgage.jpg',
-			question : 'Interest in ARM changes periodically.',
-			answer : 'A'
-		},
-		{
-			stepNo : 3,
-			title : 'Adjustment Date',
-			description : 'The date the interest rate changes on an adjustable-rate mortgage.',
-			imageUrl : 'https://loanscanada.ca/wp-content/uploads/2016/08/interest-adjustment.jpg',
-			question : 'Adjustment date is the date the initial value of a morrgae changes.',
-			answer : 'B'
-		}
-		];
+		vm.steps = [];
+		vm.steps = courseLaunchService.index($routeParams.id);
 		
-		vm.course = {
-				id : 1,
-				name : 'Real Estate Concepts 1',
-				nextStepNo : 1,
-				progress : 0,
-				dateStarted : '06/18/2017'
-			}
+		vm.course = {};
+		vm.course = courseService.show($routeParams.id);
 	
 		vm.reload = function(){			
 			vm.currentStep = vm.steps.find(x => x.stepNo == vm.course.nextStepNo);
--- ngLearningCenter/WebContent/app/course/courseList/courseList.component.js
+++ ngLearningCenter/WebContent/app/course/courseList/courseList.component.js
@@ -1,23 +1,11 @@
 angular.module('course')
 .component('courseList',{
 	templateUrl : 'app/course/courseList/courseList.component.html',
-	controller : function($location){
+	controller : function($location, courseService){
 		var vm = this;
 		
-		vm.courses = [{
-			id : 1,
-			name : 'Real Estate Concepts 1',
-			nextStepNo : 1,
-			progress : 0,
-			dateStarted : '06/18/2017'
-		}, {
-			id : 2,
-			name : 'Real Estate Concepts 2',
-			nextStepNo : 1,
-			progress : 0,
-			dateStarted : '06/18/2017'
-		}
-		];
+		vm.courses = [];
+		vm.courses = courseService.index();
 		
 		vm.launch = function(course){
 			$location.path('/course/'+course.id);
