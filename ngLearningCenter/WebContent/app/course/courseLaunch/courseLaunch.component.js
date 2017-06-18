c6d9f6d92d92cccca8277b20c7d0ed15b4dab77c
Shaun Dashjian <shaundashjian@yahoo.com>
2017-06-18 13:07:39 -0600

    Showed step image, title, and description

--- ngLearningCenter/WebContent/app/course/courseLaunch/courseLaunch.component.js
+++ ngLearningCenter/WebContent/app/course/courseLaunch/courseLaunch.component.js
@@ -5,14 +5,14 @@ angular.module('course')
 		var vm = this;
 		
 		vm.steps = [{
-			step_no : 1,
+			stepNo : 1,
 			title : 'Acceleration Clause',
 			description : 'A clause in your mortgage which allows the lender to demand payment of the outstanding loan balance for various reasons. The most common reasons for accelerating a loan are if the borrower defaults on the loan or transfers title to another individual without informing the lender.',
 			imageUrl : 'http://notequeen.com/wp-content/uploads/sites/45/2008/11/halloween.jpg',
 			question : 'The acceleration clause allow the lender to demand payment.',
 			answer : 'A'
 		}, {
-			step_no : 2,
+			stepNo : 2,
 			title : 'Adjustable-Rate Mortgage (ARM)',
 			description : 'A mortgage in which the interest changes periodically, according to corresponding fluctuations in an index. All ARMs are tied to indexes.',
 			imageUrl : 'http://www.themoneyalert.com/images/Adjustable_Rate_Mortgage.jpg',
@@ -28,6 +28,12 @@ angular.module('course')
 				progress : 0,
 				dateStarted : '06/18/2017'
 			}
+	
+		
+		vm.currentStep = vm.steps.find(x => x.stepNo == vm.course.nextStepNo);
+		
+		
+
 	},
 	controllerAs : 'vm'
 })
