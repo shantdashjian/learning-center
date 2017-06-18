5e085fab3e9319fbe69def33644b9b3e600d198a
Shaun Dashjian <shaundashjian@yahoo.com>
2017-06-18 11:26:08 -0600

    Added course list component, background, and favicon

--- ngLearningCenter/WebContent/app/course/courseList/courseList.component.js
+++ ngLearningCenter/WebContent/app/course/courseList/courseList.component.js
@@ -3,6 +3,25 @@ angular.module('course')
 	templateUrl : 'app/course/courseList/courseList.component.html',
 	controller : function(){
 		var vm = this;
+		
+		vm.courses = [{
+			id : 1,
+			name : 'Real Estate Concepts 1',
+			nextStepNo : 1,
+			progress : 0,
+			dateStarted : '06/18/2017'
+		}, {
+			id : 2,
+			name : 'Real Estate Concepts 2',
+			nextStepNo : 1,
+			progress : 0,
+			dateStarted : '06/18/2017'
+		}
+		];
+		
+		vm.open = function(course){
+			console.log(course);
+		}
 	},
 	controllerAs : 'vm'
 })
