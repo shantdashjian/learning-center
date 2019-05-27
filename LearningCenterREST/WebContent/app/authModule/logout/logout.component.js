fe844338717f6d616c66d17378078a1d1345b529
Shaun Dashjian <shaundashjian@yahoo.com>
2019-05-26 21:52:18 -0700

    Cleaned up authModule

--- LearningCenterREST/WebContent/app/authModule/logout/logout.component.js
+++ LearningCenterREST/WebContent/app/authModule/logout/logout.component.js
@@ -2,8 +2,6 @@ angular.module("authModule").component("logout", {
 	templateUrl : 'app/authModule/logout/logout.component.html',
 	controller : function(authService, $location, $cookies) {
 		var vm = this;
-		
-		
 	},
 	controllerAs : 'vm'
 });
